53d7edd9f0809daac68fcef3abed6e897f74d1ae
ROBERTO FERMINO MEDEIROS <roberto.medeiros@prof.sc.senac.br>
2025-07-31 21:07:36 -0300

    primeiro commit

---
+++ src/main/java/br/com/senac/PrimeiroBackendApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroBackendApiApplication.class, args);
+	}
+
+}
